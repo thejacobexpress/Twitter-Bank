60af9b42ef8064c83ec71a782c11da16171cfc50
milkchunks <bendod2020@gmail.com>
2024-11-14 16:57:26 -0800

    Commit

--- app/src/main/java/com/example/test/Main.kt
+++ app/src/main/java/com/example/test/Main.kt
@@ -2,23 +2,31 @@ package com.example.test
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+
+val loginKey : MutableState<String> = mutableStateOf("Jacob")
+val passwordKey : MutableState<String> = mutableStateOf("pass")
+
+val balance = mutableSetOf(1000.0)
+
+val errorMessage = mutableStateOf(false)
 
 class Main : ComponentActivity() {
 
@@ -39,13 +47,20 @@ class Main : ComponentActivity() {
                 composable("LoginPage") { LoginPage(navController) }
                 composable("CreateAccountPage") { CreateAccountPage(navController) }
                 composable("BankAccountPage") { BankAccountPage(navController) }
+                composable("DepositPage") { DepositPage(navController) }
+                composable("WithdrawPage") { WithdrawPage(navController) }
             }
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun LoginPage(navController: NavController) {
-        var text by remember { mutableStateOf("I love Denis!") }
+
+        val context = LocalContext.current
+
+        var loginText by remember { mutableStateOf("") }
+        var passwordText by remember { mutableStateOf("") }
 
         Column(
             modifier = Modifier
@@ -54,16 +69,62 @@ class Main : ComponentActivity() {
             verticalArrangement = Arrangement.Center
         ) {
 
-            Text(text)
+            OutlinedTextField(
+                modifier = Modifier
+                    .height(55.dp),
+
+                value = loginText,
+                onValueChange = { it ->
+                    loginText = it
+                },
+                label = {
+                    Text(text = "Username")
+                }
+            )
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .height(55.dp),
+
+                value = passwordText,
+                onValueChange = { it ->
+                    passwordText = it
+                },
+                label = {
+                    Text(text = "Password")
+                }
+            )
 
             Button(
                 onClick = {
-                    navController.navigate("BankAccountPage")
+
+                    if(loginKey.value == loginText && passwordKey.value == passwordText) {
+                        navController.navigate("BankAccountPage")
+                    } else if(loginKey.value == loginText && passwordKey.value != passwordText) {
+                        Toast.makeText(context, "Password is incorrect!", Toast.LENGTH_LONG).show()
+                    } else if(loginKey.value == loginText && passwordKey.value != passwordText) {
+                        Toast.makeText(context, "Username is incorrect!", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(context, "SOMETHING IS WRONG!", Toast.LENGTH_LONG).show()
+                    }
+
                 }
             ) {
                 Text(text = "Login")
-                Color(0, 0, 0)
             }
+
+            Text(
+                text = "Don't have an account?"
+            )
+
+            Button(
+                onClick = {
+                    navController.navigate("CreateAccountPage")
+                }
+            ) {
+                Text(text = "Create an Account")
+            }
+
         }
     }
 
@@ -76,6 +137,45 @@ class Main : ComponentActivity() {
 
     @Composable
     fun BankAccountPage(navController: NavController) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = balance.toString()
+            )
+
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text(text = "Deposit")
+            }
+
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text(text = "Withdraw")
+            }
+
+        }
+    }
+
+    @Composable
+    fun DepositPage(navController: NavController) {
+        Column {
+
+        }
+    }
+
+    @Composable
+    fun WithdrawPage(navController: NavController) {
         Column {
 
         }
